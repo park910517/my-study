b4f8b8ac89a869e500a16bec6dd1d7f0466efae7
park910517 <77248984+park910517@users.noreply.github.com>
2021-12-20 11:16:15 +0900

    Add files via upload

---
+++ 틀린그림찾기/test.js
@@ -0,0 +1,62 @@
+
+function blind(){
+    var img= document.getElementById("bl");
+    img.style.display="none";
+    bar();
+    
+}
+
+function blind_1(){
+    var img= document.getElementById("bl_1");
+    img.style.display="none";
+    bar();
+    
+}
+
+function blind_2(){
+    var img= document.getElementById("bl_2");
+    img.style.display="none";
+    bar();
+    
+}
+
+function blind_3(){
+    var img= document.getElementById("bl_3");
+    img.style.display="none";
+    bar();
+    
+}
+
+function blind_4(){
+    var img= document.getElementById("bl_4");
+    img.style.display="none";
+    bar();
+    
+}
+
+function blind_5(){
+    var img= document.getElementById("bl_5");
+    img.style.display="none";
+    bar();
+    
+}
+
+function blind_6(){
+    var img= document.getElementById("bl_6");
+    img.style.display="none";
+    bar();
+    
+}
+
+function blind_7(){
+    var img= document.getElementById("bl_7");
+    img.style.display="none";
+    bar();
+   
+}
+
+function bar(){
+    document.getElementById("count").value += 10;
+}
+
+
